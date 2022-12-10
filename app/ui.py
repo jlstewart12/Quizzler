7a26cc7b2520a178b9b153a34e4aad04f21f65f0
Jeremy Stewart <jlstewart12@gmail.com>
2022-12-09 21:45:35 -0500

    initializing QuizInterface

---
+++ app/ui.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+
+
+
+
